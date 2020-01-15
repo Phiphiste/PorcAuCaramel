1f4d8dac2f316174b6613913c278a97274619683
jean-leroy <jeanleroy.92500@gmail.com>
2020-01-15 13:30:18 +0100

    First Commit

---
+++ WpfApp1/WpfApp1/Model.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Model
+    {
+        public Dictionary<String, String> Countries = new Dictionary<string, string>();
+
+        public void ReadCsv(String filename)
+        {
+
+            using (var reader = new StreamReader(filename))
+            {
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    String[] values = line.Split(',');
+
+                    Countries.Add(values[0], values[1]);
+                }
+
+            }
+        }
+
+        public Model(String filename)
+        {
+            ReadCsv(filename);
+        }
+
+        public String getCap(String Country)
+        {
+            return Countries[Country];
+        }
+    }
+}
+
---
+++ WpfApp1/WpfApp1/VM.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class VM : INotifyPropertyChanged
+    {
+
+
+        public String currentCapital;
+        public Model model;
+        public ObservableCollection<Country> countries = new ObservableCollection<Country>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public VM()
+        {
+            model = new Model("C:/Users/jeanl/source/repos/WpfApp1/WpfApp1/country-capitals.csv");
+
+            foreach (KeyValuePair<string, String> c in model.Countries)
+            {
+                countries.Add(new Country(c.Key, c.Value));
+            }
+            currentCapital = "oui";
+        }
+
+        
+        public ObservableCollection<Country> Countries
+            {
+            get { return this.countries; }
+            
+            set
+            {
+                this.countries = value;
+                OnPropertyChanged("Countries");
+            }
+        }
+    }
+
+    public class Country 
+    {
+        public String name { get; set; }
+        public String capital { get; set; }
+        public Country (String n, String c)
+            {
+            name = n;
+            capital = c;
+            }
+            
+    }
+}
