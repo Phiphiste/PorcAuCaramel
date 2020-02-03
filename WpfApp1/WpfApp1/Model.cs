09b999e0c520bef80e593e05d7d3d9544810d9b0
Arnaud <arnaud.emprin@gmail.com>
2020-02-03 17:03:29 +0100

    Ajout de champs, gestion des équipes, rédaction de JSON référence

--- WpfApp1/WpfApp1/Model.cs
+++ WpfApp1/WpfApp1/Model.cs
@@ -12,6 +12,7 @@ namespace WpfApp1
     class Model
     {
         public List<Player> Players = new List<Player>();
+        public List<Team> NBATeams = new List<Team>();
 
         public void GetPlayers(string url)
         {
@@ -31,7 +32,40 @@ namespace WpfApp1
                 foreach (var player in obj.Root.results.bindings)
                 {
                     string full_name = (string)player.itemLabel.value;
-                    Players.Add(new Player(full_name));
+                    Player p = new Player(full_name);
+
+                    p.height = (int)player.hauteur.value;
+                    p.weight = (int)player.masse.value;
+                    p.position = (string)player.position_de_jeu_sp_cialit_Label.value;
+                    p.nationality = (string)player.pays_de_nationalit_Label.value;
+                    p.birthdate = (string)player.date_de_naissance.value;
+
+                    if (player.d_but_de_la_p_riode_d_activit_ != null)
+                    {
+                        string work_period_start = (string)player.d_but_de_la_p_riode_d_activit_.value;
+                        work_period_start.Substring(work_period_start.Length - 4);
+                        p.work_period_start = work_period_start;
+                    }
+                    else p.work_period_start = " ";
+
+                    if (player.fin_de_la_p_riode_d_activit_ != null)
+                    {
+                        string work_period_end = (string)player.fin_de_la_p_riode_d_activit_.value;
+                        work_period_end.Substring(work_period_end.Length - 4);
+                        p.work_period_end = work_period_end;
+                    }
+                    else p.work_period_end = " ";
+                    
+
+                    if (player.membre_de_l__quipe_de_sportLabel != null)
+                    {
+                        Team t = findTeamInNBATeams((string)player.membre_de_l__quipe_de_sportLabel.value);
+                        if(t!=null)
+                        {
+                            p.teams.Add(t);
+                            Players.Add(p);
+                        }
+                    }
                 }
             }
             data.Close();
@@ -39,8 +73,25 @@ namespace WpfApp1
             reader.Close();
         }
 
+        public Team findTeamInNBATeams(string team_name)
+        {
+            foreach(Team t in NBATeams)
+            {
+                if (t.teamName == team_name)
+                    return t;
+            }
+            return null;
+        }
+
+        public void GetTeams()
+        {
+            string json = System.IO.File.ReadAllText("../../teams.json");
+            NBATeams = JsonConvert.DeserializeObject<List<Team>>(json);
+        }
+
         public Model(String url)
         {
+            GetTeams();
             GetPlayers(url);
         }
     }
--- WpfApp1/WpfApp1/VM.cs
+++ WpfApp1/WpfApp1/VM.cs
@@ -35,13 +35,39 @@ namespace WpfApp1
 
         public VM()
         {
-            model = new Model("https://query.wikidata.org/sparql?format=json&action=wbgetentities&query=SELECT%20%3Fitem%20%3FitemLabel%20%3Fid%20WHERE%20%7B%0A%20%20SERVICE%20wikibase%3Alabel%20%7B%20bd%3AserviceParam%20wikibase%3Alanguage%20%22%5BAUTO_LANGUAGE%5D%2Cen%22.%20%7D%0A%20%20%3Fitem%20wdt%3AP31%20wd%3AQ5%3B%0A%20%20%20%20wdt%3AP106%20wd%3AQ3665646.%0A%20%20%3Fitem%20wdt%3AP2685%20%3Fid.%20%0A%7D%0ALimit%2020");
+            var url = "https://query.wikidata.org/sparql?format=json&action=wbgetentities&query=SELECT%20%3FitemLabel%20%3Fhauteur%20%3Fmasse%20%3Fposition_de_jeu_sp_cialit_Label%20%3Fpays_de_nationalit_Label%20%3Fdate_de_naissance%20%3Fmembre_de_l__quipe_de_sportLabel%20%3Fd_but_de_la_p_riode_d_activit_%20%3Ffin_de_la_p_riode_d_activit_%20WHERE%20%7B%0A%20%20SERVICE%20wikibase%3Alabel%20%7B%20bd%3AserviceParam%20wikibase%3Alanguage%20%22%5BAUTO_LANGUAGE%5D%2Cen%22.%20%7D%0A%20%20%3Fitem%20wdt%3AP31%20wd%3AQ5%3B%0A%20%20%20%20wdt%3AP106%20wd%3AQ3665646%3B%0A%20%20%20%20wdt%3AP2685%20%3Fid.%0A%20%20OPTIONAL%20%7B%20%3Fitem%20wdt%3AP2048%20%3Fhauteur.%20%7D%0A%20%20OPTIONAL%20%7B%20%3Fitem%20wdt%3AP54%20%3Fmembre_de_l__quipe_de_sport.%20%7D%0A%20%20OPTIONAL%20%7B%20%3Fitem%20wdt%3AP2067%20%3Fmasse.%20%7D%0A%20%20OPTIONAL%20%7B%20%3Fitem%20wdt%3AP413%20%3Fposition_de_jeu_sp_cialit_.%20%7D%0A%20%20OPTIONAL%20%7B%20%3Fitem%20wdt%3AP27%20%3Fpays_de_nationalit_.%20%7D%0A%20%20OPTIONAL%20%7B%20%3Fitem%20wdt%3AP569%20%3Fdate_de_naissance.%20%7D%0A%20%20OPTIONAL%20%7B%20%3Fitem%20wdt%3AP2561%20%3Fnom.%20%7D%0A%20%20OPTIONAL%20%7B%20%3Fitem%20wdt%3AP2031%20%3Fd_but_de_la_p_riode_d_activit_.%20%7D%0A%20%20OPTIONAL%20%7B%20%3Fitem%20wdt%3AP2032%20%3Ffin_de_la_p_riode_d_activit_.%20%7D%0A%7D%0ALIMIT%20100";
+            model = new Model(url);
             foreach(Player p in model.Players)
             {
-                players.Add(p);
+                Player existing_player = getPlayerFromPlayers(p.full_name);
+                if (existing_player == null)
+                    players.Add(p);
+                else
+                {
+                    if (getTeamFromTeams(existing_player.teams,p.teams.First().teamName) == null)
+                        existing_player.teams.Add(p.teams.First());
+                }
             }
+        }
 
-            selected_player = new Player("j j");
+        public Team getTeamFromTeams(List<Team> teams, string team_name)
+        {
+            foreach(Team t in teams)
+            {
+                if (t.teamName == team_name)
+                    return t;
+            }
+            return null;
+        }
+
+        public Player getPlayerFromPlayers(string full_name)
+        {
+            foreach(Player p in players)
+            {
+                if (p.full_name == full_name)
+                    return p;
+            }
+            return null;
         }
 
         public Player Selected_player
@@ -69,26 +95,33 @@ namespace WpfApp1
 
     public class Player
     {
-        public string first_name { get; set; }
-        public string last_name { get; set; }
-        public DateTime birthdate;
+        
+        public string full_name { get; set; }
+        public string birthdate;
         public int height;
         public int weight;
-        public Team team;
+        public string position { get; set; }
+        public string nationality;
+        public string work_period_start;
+        public string work_period_end;
+        public List<Team> teams = new List<Team>();
 
         public Player(string full_name)
         {
-            String[] separator = { " " };
-            String[] strlist = full_name.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-            this.first_name = strlist[0];
-            this.last_name = strlist[1];
+            this.full_name = full_name;
         }
     }
     
     public class Team
     {
-        public string name;
-        public string city;
-        public DateTime creation;
+        public string abbreviation;
+        public string teamName;
+        public string simpleName;
+        public string location;
+
+        public Team(string name)
+        {
+            this.teamName = name;
+        }
     }
 }
