9ea75e277578dfd9b0258ee4d6dd167ab5e3a9af
Arnaud <arnaud.emprin@gmail.com>
2020-02-25 16:52:11 +0100

    Récupération d'une list à afficher + navigation entre pages

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,10 +22,76 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        public int currentPageNumber = 1;
+        public int resultsByPage = 10;
+
+        //players displayed in current page
+        public List<Player> playersDisplayed = new List<Player>();
+
         public MainWindow()
         {
             InitializeComponent();
             this.DataContext = new VM();
+        }
+
+        private void SearchResults(object sender, RoutedEventArgs e)
+        {
+            //clear current list
+            ResultsListBox.Items.Clear();
+            VM v = (VM)this.DataContext;
+
+            //Fetch results from ViewModel depending on filters
+            playersDisplayed = v.players.Take(this.resultsByPage).ToList();
+
+            //Add players to the results listbox
+            foreach(Player p in playersDisplayed)
+            {
+                ResultsListBox.Items.Add(p.full_name);   
+            }
+        }
+
+        private void NextPage(object sender, RoutedEventArgs e)
+        {
+            //clear current list
+            ResultsListBox.Items.Clear();
+            VM v = (VM)this.DataContext;
+
+            //Set current page
+            currentPageNumber++;
+
+            //Selection of the corresponding sublist
+            playersDisplayed = v.players.Skip(currentPageNumber * resultsByPage)
+                                        .Take(resultsByPage).ToList();
+
+            //Add players to the results listbox
+            foreach (Player p in playersDisplayed)
+            {
+                ResultsListBox.Items.Add(p.full_name);
+            }
+        }
+
+        private void PreviousPage(object sender, RoutedEventArgs e)
+        {
+            //clear current list
+            ResultsListBox.Items.Clear();
+            VM v = (VM)this.DataContext;
+
+            //if we are at the base page we can't go previous
+            if (currentPageNumber > 1)
+            {
+                //set current page
+                currentPageNumber--;
+
+                //Selection of the corresponding sublist
+                playersDisplayed = v.players.Skip(currentPageNumber * resultsByPage)
+                                            .Take(resultsByPage).ToList();
+
+                //Add players to the results listbox
+                foreach (Player p in playersDisplayed)
+                {
+                    ResultsListBox.Items.Add(p.full_name);
+                }
+            }
         }
 
         /*private void Rechercher(object sender, RoutedEventArgs e)
