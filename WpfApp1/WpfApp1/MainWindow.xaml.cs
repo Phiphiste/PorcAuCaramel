cf0b724f9d54a506c39f85a7745a120e24110c8d
Arnaud <arnaud.emprin@gmail.com>
2020-02-25 19:21:51 +0100

    Amélioration du scrolling entre les pages

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -22,11 +22,15 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
-        public int currentPageNumber = 1;
+        public int currentPageNumber = 0;
         public int resultsByPage = 10;
+        public int numberOfPages;
 
         //players displayed in current page
         public List<Player> playersDisplayed = new List<Player>();
+
+        //All players corresponding to research
+        public List<Player> fullQueryResult = new List<Player>();
 
         public MainWindow()
         {
@@ -34,63 +38,64 @@ namespace WpfApp1
             this.DataContext = new VM();
         }
 
-        private void SearchResults(object sender, RoutedEventArgs e)
+        public void DisplayCurrentPage()
         {
             //clear current list
             ResultsListBox.Items.Clear();
-            VM v = (VM)this.DataContext;
 
             //Fetch results from ViewModel depending on filters
-            playersDisplayed = v.players.Take(this.resultsByPage).ToList();
-
-            //Add players to the results listbox
-            foreach(Player p in playersDisplayed)
-            {
-                ResultsListBox.Items.Add(p.full_name);   
-            }
-        }
-
-        private void NextPage(object sender, RoutedEventArgs e)
-        {
-            //clear current list
-            ResultsListBox.Items.Clear();
-            VM v = (VM)this.DataContext;
-
-            //Set current page
-            currentPageNumber++;
-
-            //Selection of the corresponding sublist
-            playersDisplayed = v.players.Skip(currentPageNumber * resultsByPage)
-                                        .Take(resultsByPage).ToList();
+            playersDisplayed = fullQueryResult.Skip(currentPageNumber * resultsByPage)
+                                              .Take(resultsByPage).ToList();
 
             //Add players to the results listbox
             foreach (Player p in playersDisplayed)
             {
                 ResultsListBox.Items.Add(p.full_name);
             }
+
+            //if it's the first page, disable "Previous" button
+            if (currentPageNumber == 0)
+                PreviousBtn.IsEnabled = false;
+            else PreviousBtn.IsEnabled = true;
+
+            //if it's the last page, disable "Next" button
+            if ((currentPageNumber+1) == numberOfPages)
+                NextBtn.IsEnabled = false;
+            else NextBtn.IsEnabled = true;
+
+            //Display current page number
+            CurrentPageTextBlock.Text = "Page " + (currentPageNumber + 1).ToString() + " of " + numberOfPages.ToString();
+        }
+
+        private void SearchResults(object sender, RoutedEventArgs e)
+        {
+            VM v = (VM)this.DataContext;
+            //allPlayers = v.getResearchResult();
+            fullQueryResult = v.players.ToList();
+            numberOfPages = (fullQueryResult.Count() / resultsByPage) + 1;
+
+            ResultsPanel.Visibility = Visibility.Visible;
+
+            DisplayCurrentPage();
+        }
+
+        private void NextPage(object sender, RoutedEventArgs e)
+        {
+            //Set current page
+            currentPageNumber++;
+
+            DisplayCurrentPage();
         }
 
         private void PreviousPage(object sender, RoutedEventArgs e)
         {
-            //clear current list
-            ResultsListBox.Items.Clear();
-            VM v = (VM)this.DataContext;
-
             //if we are at the base page we can't go previous
-            if (currentPageNumber > 1)
+            if (currentPageNumber > 0)
             {
                 //set current page
                 currentPageNumber--;
 
-                //Selection of the corresponding sublist
-                playersDisplayed = v.players.Skip(currentPageNumber * resultsByPage)
-                                            .Take(resultsByPage).ToList();
-
-                //Add players to the results listbox
-                foreach (Player p in playersDisplayed)
-                {
-                    ResultsListBox.Items.Add(p.full_name);
-                }
+                DisplayCurrentPage();
             }
         }
 
