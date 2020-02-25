4bda0612c5da29e3e3f9563809b4b7052018eb24
pramb <pramb@LAPTOP-6AU06JTG>
2020-02-25 11:20:15 +0100

    CardWindow setup and links

--- WpfApp1/WpfApp1/CardWindow.xaml.cs
+++ WpfApp1/WpfApp1/CardWindow.xaml.cs
@@ -10,18 +10,26 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WpfApp1
 {
     /// <summary>
-    /// Logique d'interaction pour CardWindow.xaml
+    /// Logique d'interaction pour MainWindow.xaml
     /// </summary>
     public partial class CardWindow : Window
     {
+        // Constructor
         public CardWindow()
         {
             InitializeComponent();
+            this.DataContext = new VM();
+        }
+
+        public void DisplayCard()
+        {
+
         }
     }
 }
